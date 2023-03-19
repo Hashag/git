c0860414705242649eeb360f25bca5038e130023
ohtoai <abcdekl35@gmail.com>
2023-03-19 15:05:05 +0800

    master commit 1

---
+++ src/main/java/com/rikka/Git.java
@@ -0,0 +1,11 @@
+package com.rikka;
+
+/**
+ * @author OhtoAi
+ * 2023/3/19 15:04
+ */
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("master version 1");
+    }
+}
